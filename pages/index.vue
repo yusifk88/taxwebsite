ca59667eb772590288ed2767daf747b551d05c46
anasnanjo <anasnanjo@zerotrustmsp.tech>
2024-06-12 18:30:39 -0500

    Update index.vue
    tags added

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
-  <head><!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TNCNSQRZ');</script>
-<!-- End Google Tag Manager -->
+  <head>
+    <!-- End Google Tag Manager -->
   <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-DJTJKJP10Y"></script>
 <script>
@@ -15,8 +10,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
   gtag('config', 'G-DJTJKJP10Y');
 </script>
-
-  </head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TNCNSQRZ');</script>
+ </head>
   <home-jombothrone></home-jombothrone>
 
   <v-container class="pr-0 mt-6">
