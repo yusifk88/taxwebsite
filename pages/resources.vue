632547bf99d0ed84c90a0eca81d1188be4d2bcad
anasnanjo <anasnanjo@zerotrustmsp.tech>
2024-06-12 17:46:48 -0500

    Update resources.vue

--- pages/resources.vue
+++ pages/resources.vue
@@ -119,3 +119,12 @@ export default defineComponent({
 <style scoped>
 
 </style>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DJTJKJP10Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DJTJKJP10Y');
+</script>
